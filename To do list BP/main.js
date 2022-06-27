6551175e6956b25e7ee18d7653e2d25f23ca2af6
Undome <arkdan@gmail.com>
2022-06-27 00:14:43 -0500

    Reviewed, compleated and ready to go

--- To do list BP/main.js
+++ To do list BP/main.js
@@ -2,8 +2,16 @@ let addToDoButton = document.getElementById('addToDo');
 let toDoContainer = document.getElementById('toDoContainer');
 let inputField = document.getElementById('inputField');
 
-addToDoButton.addEventListener('click', functions(){
-    var paragraph = document.createElement('p')
+addToDoButton.addEventListener('click', function(){
+    var paragraph = document.createElement('p');
+    paragraph.classList.add('paragraph-styling');
     paragraph.innerText = inputField.value;
     toDoContainer.appendChild(paragraph);
+    inputField.value = '';
+    paragraph.addEventListener('click', function() {
+        paragraph.style.textDecoration = "line-through";
+    })
+    paragraph.addEventListener('dbclick', function() {
+        paragraph.removeChild(paragraph);
+    })
 })
