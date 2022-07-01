ad9a1fa9cf20b8aa083fedd22618fdc8ace32f33
Undome <arkdan@gmail.com>
2022-06-30 23:54:38 -0500

    Add delet button and li functionality

--- Lista de tareas/main.js
+++ Lista de tareas/main.js
@@ -5,4 +5,24 @@ const empty = document.querySelector('.empty');
 
 addBtn.addEventListener('click', (e) => {
     e.preventDefault();
+
+    const text = input.value;
+    const li = document.createElement('li');
+    const p = document.createElement('p');
+    p.textContent = text;
+    li. appendChild(p);
+    li.appendChild(addDeletBtn());
+    ul.appendChild(li);
+    input.value = '';
 })
+
+function addDeletBtn () {
+    const deletBtn = document.createElement('button');
+    deletBtn.textContent = 'X';
+    deletBtn.className = 'btn-delete';
+    deletBtn.addEventListener('click', (e) => {
+        const item = e.target.parentElement;
+        ul.removeChild(item);
+    }); 
+    return
+}
